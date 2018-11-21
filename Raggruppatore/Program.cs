a52cf725417af7796d5a3351f3445eb1dd587102
alessandro leonori <aleonori@yahoo.com>
2018-11-21 08:52:46 +0100

    Aggiungere i file di progetto.

---
+++ Raggruppatore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raggruppatore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
